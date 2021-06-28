2f9483e7f821ecda063c8369e618298219ac7a68
phene <60532517+joebobbio@users.noreply.github.com>
2021-06-27 22:47:39 -0400

    bugfixes
    directory did not have to be created as root, added "grey" as an accepted spelling for "gray"

--- setbar.sh
+++ setbar.sh
@@ -2,15 +2,14 @@
 
 #This part will ask you for the Colors
 echo -e "\e[31m=Welcome to the Color Setter!=";
-echo -e "\e[32mDefault Colors: cyan, green, blue, red, black, gray, yellow, reset"
-echo "Note: Color reset will return to default color"
-echo -e "\e[0mExample Color: #6fe8be"
+echo -e "\e[32mDefault Colors: cyan, green, blue, red, black, gray, yellow, or reset to revert the color to the default."
+echo -e "\e[0mExample custom color: #6fe8be"
 echo -e "\e[0mPlease type a color in HEX or type one of the defaults listed above that will set your toolbar color: "
 read clr;
 
 strToMatch="exit"
 
-sudo mkdir -p ~/.config/systemd/user/sommelier-x@0.service.d
+mkdir -p ~/.config/systemd/user/sommelier-x@0.service.d
 
 # #f2f2f2 (default)
 #
@@ -43,44 +42,44 @@ elif [ "$clr" == "green" ] || [ "$clr" == "Green" ]
 then 
     echo -e '[Service]\nEnvironment="SOMMELIER_FRAME_COLOR=#98c379"' > ~/.config/systemd/user/sommelier-x@0.service.d/override.conf
 
-    echo "Color set to default [green]. Please restart the Linux Environment for changes to take effect"
+    echo "Color set to default [green]. Please restart the Linux Environment for changes to take effect."
 elif [ "$clr" == "reset" ] || [ "$clr" == "default" ]
 then
     echo -e '[Service]\nEnvironment="SOMMELIER_FRAME_COLOR=#f2f2f2"' > ~/.config/systemd/user/sommelier-x@0.service.d/override.conf
 
-    echo "Color set to default [reset]. Please restart the Linux Environment for changes to take effect"
+    echo "Color set to default [reset]. Please restart the Linux Environment for changes to take effect."
 elif [ "$clr" == "cyan" ] || [ "$clr" == "Cyan" ]
 then
     echo -e '[Service]\nEnvironment="SOMMELIER_FRAME_COLOR=#56B6C2"' > ~/.config/systemd/user/sommelier-x@0.service.d/override.conf
 
-    echo "Color set to default [cyan]. Please restart the Linux Environment for changes to take effect"
+    echo "Color set to default [cyan]. Please restart the Linux Environment for changes to take effect."
 elif [ "$clr" == "red" ] || [ "$clr" == "Red" ]
 then
     echo -e '[Service]\nEnvironment="SOMMELIER_FRAME_COLOR=#E06C75"' > ~/.config/systemd/user/sommelier-x@0.service.d/override.conf
 
-    echo "Color set to default [red]. Please restart the Linux Environment for changes to take effect"
+    echo "Color set to default [red]. Please restart the Linux Environment for changes to take effect."
 elif [ "$clr" == "blue" ] || [ "$clr" == "Blue" ]
 then
     echo -e '[Service]\nEnvironment="SOMMELIER_FRAME_COLOR=#61AFEF"' > ~/.config/systemd/user/sommelier-x@0.service.d/override.conf
 
-    echo "Color set to default [blue]. Please restart the Linux Environment for changes to take effect"
+    echo "Color set to default [blue]. Please restart the Linux Environment for changes to take effect."
 elif [ "$clr" == "black" ] || [ "$clr" == "Black" ]
 then
     echo -e '[Service]\nEnvironment="SOMMELIER_FRAME_COLOR=#282c34"' > ~/.config/systemd/user/sommelier-x@0.service.d/override.conf
 
-    echo "Color set to default [black]. Please restart the Linux Environment for changes to take effect"
+    echo "Color set to default [black]. Please restart the Linux Environment for changes to take effect."
 elif [ "$clr" == "yellow" ] || [ "$clr" == "Yellow" ]
 then
     echo -e '[Service]\nEnvironment="SOMMELIER_FRAME_COLOR=#E5c07B"' > ~/.config/systemd/user/sommelier-x@0.service.d/override.conf
 
-    echo "Color set to default [yellow]. Please restart the Linux Environment for changes to take effect"
-elif [ "$clr" == "gray" ] || [ "$clr" == "Gray" ]
+    echo "Color set to default [yellow]. Please restart the Linux Environment for changes to take effect."
+elif [ "$clr" == "gray" ] || [ "$clr" == "Gray" ] || [ "$clr" == "grey" ] || [ "$clr" == "Grey" ]
 then
     echo -e '[Service]\nEnvironment="SOMMELIER_FRAME_COLOR=#ABB2BF"' > ~/.config/systemd/user/sommelier-x@0.service.d/override.conf
 
-    echo "Color set to default [gray]. Please restart the Linux Environment for changes to take effect"
+    echo "Color set to default [gray]. Please restart the Linux Environment for changes to take effect."
 else 
     echo -e "[Service]\nEnvironment=SOMMELIER_FRAME_COLOR=$clr" > ~/.config/systemd/user/sommelier-x@0.service.d/override.conf
 
-    echo "Color set to custom [$clr]. Please retart the Linux Environment for changes to take effect"
+    echo "Color set to custom [$clr]. Please retart the Linux Environment for changes to take effect."
 fi
